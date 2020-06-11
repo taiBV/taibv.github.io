bdc8901cdbf3becb61103314347494458d9a9074
taibv <chutai3212u@gmail..com>
2020-06-11 11:15:03 +0700

    up

---
+++ js/config.js
@@ -0,0 +1,6 @@
+
+    var id_form=79;
+    function config_form(){
+        var scr=`<script type="text/javascript" src="https://form.edupia.vn/form/generate.js?id=79"></script>`;
+        return scr;
+    }
