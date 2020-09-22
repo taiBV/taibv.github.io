2f30feaf6aebadb388c53ae34494e29bbacf8722
taibv <taibv@watermelon.vn>
2020-09-22 23:36:16 +0700

    updaste

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$( function() {
+    lazyload();
+    $( "#datepicker" ).datepicker({
+        dateFormat: 'dd-mm-yy',
+    });
+    $( "#datepicker" ).datepicker().datepicker("setDate", new Date());
+    $(".hambuger").click(function () {
+        $(".sub_menu_mobi").toggle("slide");
+    })
+    $(".icon-close").click(function () {
+        $(".sub_menu_mobi").toggle("slide");
+    })
+} );
+function showDetail(){
+    $(".info_detail_search").slideToggle( "slow" );
+};
+$('.carousel').carousel({
+    interval: false,
+});
